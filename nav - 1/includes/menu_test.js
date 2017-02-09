83fc76d3989ce25d3f5faa0207811193c51629e0
shirelylee <shilijob@gmail.com>
2017-02-08 20:29:17 -0500

    Add files via upload

---
+++ nav - 1/includes/menu_test.js
@@ -0,0 +1,13 @@
+$(document).ready(function() {
+
+    $('.nav_menu .dropmenu').css('display', 'block');
+    $('.nav_menu > ul').dropmenu({
+        effect: 'slide',
+        speed: 250,
+        timeout: 0,
+        nbsp: false
+    });
+
+
+
+});
